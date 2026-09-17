6747aebc9de90c7242ad1e08fc14940895e5de4f
manzanoiezer-ops <manzano.iezer@gmail.com>
2026-09-17 15:57:46 +0800

    one line

--- obj/Debug/net9.0/EPortfolio.AssemblyInfo.cs
+++ obj/Debug/net9.0/EPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e2924c41738517007bf05732e938bdb9b8a3366")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dc81c77a55a2649ab9e3f0fb32480056d11f972")]
 [assembly: System.Reflection.AssemblyProductAttribute("EPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
